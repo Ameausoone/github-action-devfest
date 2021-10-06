7341c9ef665de3fc8e4ce2362a62539ee9d3bd55
Antoine Meausoone <592978+Ameausoone@users.noreply.github.com>
2021-10-06 07:32:26 +0200

    feat(1st part): start organization

--- docs/scripts/slides.js
+++ docs/scripts/slides.js
@@ -1,17 +1,37 @@
 // One method per module
-function schoolSlides() {
-  return ['00-school/00-TITLE.md', '00-school/speaker-jef.md'];
+function intro() {
+  return ['01-Intro/00-title.md', '01-Intro/speaker-ame.md','01-Intro/02-experience.md'];
 }
 
-function introSlides() {
-  return ['intro/00-TITLE.md'];
+function cicd_journey() {
+  return ['02-cicd-journey/00-small-history.md']
+}
+
+function then_github_action() {
+  return ['03-then-github-action/00-intro.md','03-then-github-action/01-how-it-works.md','03-then-github-action/02-actions.md']
+}
+
+function github_actions_use_it() {
+  return ['04a-github-actions-use-it/']
+}
+
+function github_actions_how_to_write() {
+  return ['04b-github-actions-how-to-write/']
+}
+
+function github_actions_advise_to_write() {
+  return ['04c-github-actions-advise-to-write/']
 }
 
 function formation() {
   return [
     //
-    ...schoolSlides(), //
-    ...introSlides() //
+    ...intro(),
+    ...cicd_journey(),
+    ...then_github_action(),
+    ...github_actions_use_it(),
+    ...github_actions_how_to_write(),
+    ...github_actions_advise_to_write()
   ].map(slidePath => {
     return { path: slidePath };
   });
