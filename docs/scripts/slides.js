cc7cd82ff6f9360a4a972f8d0b3580c53ea697d2
Romain Lespinasse <romain.lespinasse@gmail.com>
2023-09-12 20:51:11 +0200

    make ready for BBL AXA 2023-09-12

--- docs/scripts/slides.js
+++ docs/scripts/slides.js
@@ -1,7 +1,7 @@
 import { SfeirThemeInitializer } from '../web_modules/sfeir-school-theme/sfeir-school-theme.mjs';
 
 // One method per module
-// TODO add Romain
+
 function intro() {
   return [
     '01-Intro/00-title.md',
@@ -26,13 +26,9 @@ function then_github_action() {
   ]
 }
 
-// TODO remove demo
-
 function github_actions_use_it() {
   return [
     '04a-github-actions-use-it/00-what-is.md',
-    //'04a-github-actions-use-it/01-demo.md',
-    //'04a-github-actions-use-it/01-demo-b.md',
     '04a-github-actions-use-it/02-security.md'
   ]
 }
@@ -44,8 +40,6 @@ function github_actions_how_to_write() {
     '04b-github-actions-how-to-write/02-github-slug-action-impl.md'
   ]
 }
-
-// TODO add new features
 
 function finish() {
   return ['05-end/00-finish.md']
